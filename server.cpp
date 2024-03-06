fcd02fa0e1018d7de94590a392262028f37a7ac3
Andres Santiago Orozco Gorjon <asogbu@santis-mbp.dhcp.nd.edu>
2024-03-06 02:45:22 -0500

    Fix definitions for compilation in VM

--- server.cpp
+++ server.cpp
@@ -106,10 +106,10 @@ int socket_listen(const char *port) {
 
     /* Lookup server address information */
     struct addrinfo hints = {
-        .ai_flags = (AI_V4MAPPED_CFG | AI_ADDRCONFIG | AI_PASSIVE),  // Use default flags and config socket for bind
-        .ai_family = AF_UNSPEC,                                      // Any address family (IPv4 or IPv6)
-        .ai_socktype = SOCK_STREAM,                                  // Full-duplex byte stream (TCP)
-        .ai_protocol = IPPROTO_TCP                                   // TCP protocol
+        .ai_flags = (AI_V4MAPPED | AI_ADDRCONFIG | AI_PASSIVE),  // Use default flags and config socket for bind
+        .ai_family = AF_UNSPEC,                                  // Any address family (IPv4 or IPv6)
+        .ai_socktype = SOCK_STREAM,                              // Full-duplex byte stream (TCP)
+        .ai_protocol = IPPROTO_TCP                               // TCP protocol
     };
     struct addrinfo *results;
 
